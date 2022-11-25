e0a317b18aab1d0756bc6bb5ca430dea33f8cd3b
3x0d2s-msi <exodus.yarwood@icloud.com>
2022-11-25 16:33:25 +0300

    2022-11-25 Папка базы данных

--- bot/tgbot/data/db_dump.sql
+++
@@ -1,7 +0,0 @@
-
--- Таблица: Users
-CREATE TABLE IF NOT EXISTS Users (
-    id SERIAL PRIMARY KEY,
-    user_id BIGINT NOT NULL
-);
-
---
+++ db/db_dump.sql
@@ -0,0 +1,12 @@
+
+-- Таблица: Users
+CREATE TABLE IF NOT EXISTS Users (
+    id SERIAL PRIMARY KEY,
+    tg_id BIGINT NOT NULL,
+    mail VARCHAR(100) NOT NULL,
+    name VARCHAR(60) NOT NULL,
+    surname VARCHAR(60) NOT NULL,
+    patronymic VARCHAR(60) NOT NULL,
+    group_name VARCHAR(10) NOT NULL
+);
+
