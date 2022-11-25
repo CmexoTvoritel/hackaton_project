cf65a6ea2eaad70fb340156207d5e41fff891f14
3x0d2s-msi <exodus.yarwood@icloud.com>
2022-11-25 16:51:05 +0300

    2022-11-25 Сделал структуру таблицы преподов

--- db/db_dump.sql
+++ db/db_dump.sql
@@ -10,3 +10,11 @@ CREATE TABLE IF NOT EXISTS Users (
     group_name VARCHAR(10) NOT NULL
 );
 
+-- Таблица: Teachers
+CREATE TABLE IF NOT EXISTS Users (
+    id SERIAL PRIMARY KEY,
+    mail VARCHAR(100) NOT NULL,
+    name VARCHAR(60) NOT NULL,
+    surname VARCHAR(60) NOT NULL,
+    patronymic VARCHAR(60) NOT NULL
+);
