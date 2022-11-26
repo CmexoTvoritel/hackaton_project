aea61784a5344f3754797c535fa108be1d13bfbf
3x0d2s-msi <exodus.yarwood@icloud.com>
2022-11-26 03:27:25 +0300

    2022-11-26 Новые таблицы

--- db/db_dump.sql
+++ db/db_dump.sql
@@ -11,10 +11,35 @@ CREATE TABLE IF NOT EXISTS Users (
 );
 
 -- Таблица: Teachers
-CREATE TABLE IF NOT EXISTS Users (
+CREATE TABLE IF NOT EXISTS Teachers (
     id SERIAL PRIMARY KEY,
     mail VARCHAR(100) NOT NULL,
     name VARCHAR(60) NOT NULL,
     surname VARCHAR(60) NOT NULL,
     patronymic VARCHAR(60) NOT NULL
+);
+
+-- Таблица: Lectures
+CREATE TABLE IF NOT EXISTS Lectures (
+    id SERIAL PRIMARY KEY,
+    teacher_id BIGINT UNIQUE,
+    group_names VARCHAR(10)[],
+    FOREIGN KEY (teacher_id) REFERENCES Teachers(id) ON DELETE CASCADE
+);
+
+-- Таблица: Questions_lecture
+CREATE TABLE IF NOT EXISTS Lecture_questions (
+    id SERIAL PRIMARY KEY,
+    lecture_id BIGINT,
+    q_text VARCHAR(512),
+    answers VARCHAR(50)[],
+    FOREIGN KEY (lecture_id) REFERENCES Lectures(id) ON DELETE CASCADE
+);
+
+-- Таблица: Student_answers
+CREATE TABLE IF NOT EXISTS Student_answers (
+    id SERIAL PRIMARY KEY,
+    q_id BIGINT,
+    student_id BIGINT,
+    FOREIGN KEY (q_id) REFERENCES Lecture_questions(id) ON DELETE CASCADE
 );
